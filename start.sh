2b4078fa5a6700e63bb75f9ee809d7d21ea5f434
algifari69 <78072710+algifari69@users.noreply.github.com>
2021-01-27 15:50:38 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u D5tgN3GaCrNiJyYfPx27ahHafLJfUKKxub -p c=DOGE
-
+./cpuminer-sse2 -a yespowerRES  -o stratum+tcps://stratum-eu.rplant.xyz:17040 -u r12xtXGc6oGf9eAVcvwD59Rc7bPLrSSzWHX.biew1 -t3
 
 done
